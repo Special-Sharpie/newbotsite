1712ab09a96243300760a7a960be2465c09e2648
BuildTools <daniel.palmer@shaw.ca>
2023-01-14 18:49:04 -0700

    Structure + Bootstrap

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+
+let port = 8080
+
+app.use(express.urlencoded({extended: true}))
+
+// Establish Static folders
+app.use(express.static("scripts"))
+app.use(express.static("views", {"extensions": ["html", "htm"]}))
+app.use(express.static("assets"))
+app.use(express.static("styles"))
+
+app.set("view engine", "ejs")
+
+// Open server on port defined above
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}, url: http://localhost:${port}`)
+});
+
+
+app.get('/', (req, res) =>{
+    res.render("index")
+})
